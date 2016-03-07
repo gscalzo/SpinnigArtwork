ac12cb660424d84377bbfcc516747eb11e9ff46a
Giordano Scalzo <giordano.scalzo@gmail.com>
2016-03-07 18:28:27 +0000

    FIRST IMPORT

---
+++ SpinnigArtwork/SpinningArtworkViewController.swift
@@ -0,0 +1,21 @@
+//
+//  Copyright © 2016 Giordano Scalzo. All rights reserved.
+//
+
+import UIKit
+
+class SpinningArtworkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
